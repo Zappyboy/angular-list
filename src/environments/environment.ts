d8598af156ab7dec407d9348910151dcb86b54b7
ZappF <helloworld.zapp@gmail.com>
2022-01-19 10:16:27 +0100

    connection to firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'zapp-list',
+    appId: '1:533257996939:web:8315ac71023b51205ce28e',
+    storageBucket: 'zapp-list.appspot.com',
+    apiKey: 'AIzaSyASqfJufC7bDQfX5IkSR95gVXKHxni4DEM',
+    authDomain: 'zapp-list.firebaseapp.com',
+    messagingSenderId: '533257996939',
+    measurementId: 'G-6H5504SERT',
+  },
   production: false
 };
 
